58f4e1661aa8d6689c5354b2bcf116798fd27b87
Stanislav Pivovartsev <ssp@nxc.no>
2017-11-28 18:00:25 +0200

    Trying to send emails

--- src/AppBundle/Command/SearchMenCommand.php
+++ src/AppBundle/Command/SearchMenCommand.php
@@ -3,6 +3,7 @@ namespace AppBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SearchMenCommand extends ContainerAwareCommand
@@ -10,7 +11,10 @@ class SearchMenCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this->setName('search:men')
-            ->addArgument('menLimit', null, '', 1)
+            ->addArgument('menLimit', null, '', 25)
+            ->addOption('agent', 'a', InputOption::VALUE_OPTIONAL)
+            ->addOption('staff', 's', InputOption::VALUE_OPTIONAL)
+            ->addOption('password', 'p', InputOption::VALUE_OPTIONAL)
             ->setDescription('Search men');
     }
 
@@ -22,7 +26,13 @@ class SearchMenCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $result = $this->getContainer()->get('app.service.search_men')->searchMen($input->getArgument('menLimit'));
+        $result = $this->getContainer()->get('app.service.search_men')
+                        ->searchMen(
+                            $input->getArgument('menLimit'),
+                            $input->getOption('agent') ? $input->getOption('agent') : $this->getContainer()->getParameter('default_agent'),
+                            $input->getOption('staff') ? $input->getOption('staff') : $this->getContainer()->getParameter('default_staff'),
+                            $input->getOption('password') ? $input->getOption('password') : $this->getContainer()->getParameter('default_password')
+                        );
 
         print_r($result);
 
--- src/AppBundle/Service/SearchMen.php
+++ src/AppBundle/Service/SearchMen.php
@@ -12,6 +12,11 @@ namespace AppBundle\Service;
 class SearchMen
 {
     private $siteUrl = 'http://www.charmdate.com';
+
+    private $agent;
+    private $staff;
+    private $password;
+
     /**
      * @var int
      */
@@ -27,15 +32,29 @@ class SearchMen
      */
     private $cookie;
 
-    public function searchMen(int $menLimit)
+    /**
+     * @param int $menLimit
+     * @param string $agent
+     * @param string $staff
+     * @param string $password
+     * @return array
+     */
+    public function searchMen(int $menLimit, string $agent, string $staff, string $password)
     {
         $this->menLimit = $menLimit;
+        $this->agent = $agent;
+        $this->staff = $staff;
+        $this->password = $password;
+
         $this->cookie = $this->login();
 
-        return $this->getMenWithCredits(
-            $this->getMenIds($nextPage),
-            $nextPage
-        );
+        $menWithCredits = $this->getMenWithCredits();
+
+        foreach ($menWithCredits as $manWithCredit) {
+            $this->sendEmail($manWithCredit, $this->ladyId);
+        }
+
+        return $menWithCredits;
     }
 
     /**
@@ -50,22 +69,22 @@ class SearchMen
         curl_setopt(
             $ch,
             CURLOPT_POSTFIELDS,
-            "agentid=C804&staff_id=S59241&passwd=121427111&agentlogin=Login"
+            "agentid=" . $this->agent . "&staff_id=" . $this->staff . "&passwd=" . $this->password . "&agentlogin=Login"
         );
 
         curl_setopt(
             $ch,
             CURLOPT_HTTPHEADER,
-            array(
+            [
                 'Upgrade-Insecure-Requests: 1',
                 'Referer:'.$this->siteUrl.'/clagt/loginb.htm',
                 'Origin:'.$this->siteUrl,
                 'Connection:keep-alive',
                 'User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-            )
+            ]
         );
 
-// receive server response ...
+        // receive server response ...
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_VERBOSE, 1);
         curl_setopt($ch, CURLOPT_HEADER, 1);
@@ -84,39 +103,77 @@ class SearchMen
     }
 
     /**
-     * @param $nextPage
      * @return array
      */
-    private function getMenIds(&$nextPage): array
+    private function getMenWithCredits(): array {
+        $menWithCredits = [];
+
+        do {
+            $menWithCredits = array_merge(
+                $menWithCredits,
+                $this->getMenWithCreditsByIds($this->getMenIdsByPage($page))
+            );
+
+            if (count($menWithCredits) >= $this->menLimit) {
+                break;
+            }
+
+        } while (!empty($page));
+
+        return $menWithCredits;
+    }
+
+    /**
+     * @param $page
+     * @return array
+     */
+    private function getMenIdsByPage(&$page): array
     {
         $ch = curl_init();
+
+        $now = new \DateTime();
+
+        $loginParams = [
+            'womanId' => $this->ladyId,
+            'ddddddddddddddddd' => 1,
+            'age1' => 1983,
+            'age2' => 1955,
+            'country' => '',
+            'dateType' => 'last_login',
+            'year_s' => $now->format('Y'),
+            'month_s' => $now->format('m'),
+            'day_s' => $now->format('d'),
+            'year_e' => $now->format('Y'),
+            'month_e' => $now->format('m'),
+            'day_e' => $now->format('d'),
+            'photo' => 'N',
+            'birthday' => 0,
+            'Submit' => 'Search',
+        ];
+
+        if (!empty($page)) {
+            $loginParams['page'] = $page;
+        }
 
         curl_setopt(
             $ch,
             CURLOPT_URL,
-            $this->siteUrl."/clagt/admire/search_matches3.php?womanid=".$this->ladyId."&ddddddddddddddddd=1&age1=1983&age2=1955&country=&dateType=last_login&year_s=2017&month_s=11&day_s=26&year_e=2017&month_e=11&day_e=26&photo=N&birthday=0&Submit=Search"
-        );
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt(
-            $ch,
-            CURLOPT_POSTFIELDS,
-            "agentid=C804&staff_id=S59241&passwd=121427111&agentlogin=Login"
+            $this->siteUrl."/clagt/admire/search_matches3.php?".http_build_query($loginParams)
         );
 
         curl_setopt(
             $ch,
             CURLOPT_HTTPHEADER,
-            array(
+            [
                 'Upgrade-Insecure-Requests: 1',
                 'Referer:'.$this->siteUrl.'/clagt/loginb.htm',
                 'Origin:'.$this->siteUrl,
                 'Connection:keep-alive',
                 'User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-                'Cookie:PHPSESSID='.$this->cookie
-            )
+                'Cookie:PHPSESSID='.$this->cookie.'; CD_Change_Screen=1920;'
+            ]
         );
 
-// receive server response ...
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_VERBOSE, 1);
 
@@ -130,76 +187,11 @@ class SearchMen
 
         preg_match('/page=(.+?)"/', $response, $pageMatches);
 
-        $nextPage = $pageMatches[1];
+        $page = $pageMatches[1];
 
         curl_close($ch);
 
         return $manIds;
-    }
-
-    /**
-     * @param $menIds
-     * @param $nextPage
-     * @return array
-     */
-    private function getMenWithCredits(
-        $menIds,
-        $nextPage
-    ): array {
-        $menWithCredits = [];
-
-        do {
-            $menWithCredits = array_merge(
-                $menWithCredits,
-                $this->getMenWithCreditsByIds($menIds)
-            );
-
-            if (count($menWithCredits) >= $this->menLimit) {
-                break;
-            }
-
-            $ch = curl_init();
-
-            curl_setopt(
-                $ch,
-                CURLOPT_URL,
-                $this->siteUrl."/clagt/admire/search_matches3.php?womanid=". $this->ladyId . "&ddddddddddddddddd=1&age1=1983&age2=1955&country=&dateType=last_login&year_s=2017&month_s=11&day_s=26&year_e=2017&month_e=11&day_e=26&photo=N&birthday=0&Submit=Search&page=".$nextPage
-            );
-
-            curl_setopt(
-                $ch,
-                CURLOPT_HTTPHEADER,
-                array(
-                    'Upgrade-Insecure-Requests: 1',
-                    'Referer:'.$this->siteUrl.'/clagt/loginb.htm',
-                    'Origin:'.$this->siteUrl,
-                    'Connection:keep-alive',
-                    'User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-                    'Cookie:PHPSESSID='.$this->cookie.'; CD_Change_Screen=1920;'
-                )
-            );
-
-// receive server response ...
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_VERBOSE, 1);
-
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-            $response = curl_exec($ch);
-
-            preg_match_all('/men_profile.php\?manid=(\w+)&/', $response, $matches);
-            $menIds = array_unique($matches[1]);
-            sort($menIds);
-
-            preg_match('/page=(.+?)"/', $response, $pageMatches);
-
-            $nextPage = $pageMatches[1];
-
-            curl_close($ch);
-
-        } while (!empty($nextPage));
-
-        return $menWithCredits;
     }
 
     /**
@@ -222,23 +214,24 @@ class SearchMen
             curl_setopt(
                 $ch,
                 CURLOPT_HTTPHEADER,
-                array(
+                [
                     'Upgrade-Insecure-Requests: 1',
                     'Referer:'.$this->siteUrl.'/clagt/loginb.htm',
                     'Origin:'.$this->siteUrl,
                     'Connection:keep-alive',
                     'User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
                     'Cookie:PHPSESSID='.$this->cookie
-                )
+                ]
             );
 
-// receive server response ...
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_VERBOSE, 1);
 
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
             $response = curl_exec($ch);
+
+            curl_close($ch);
 
             preg_match('/Credit: (.+?)<\/td>\s*?<td>(.+?)<\/td>/', $response, $matchesCredit);
 
@@ -252,5 +245,42 @@ class SearchMen
         }
 
         return $menWithCredits;
+    }
+
+    /**
+     * @param string $manId
+     * @param string $womanId
+     */
+    private function sendEmail(string $manId = 'CM32985282', string $womanId = 'C999099')
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $this->siteUrl."/clagt/admire/send_admire_mail2.php");
+        curl_setopt($ch, CURLOPT_POST, 1);
+
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            [
+                'Upgrade-Insecure-Requests: 1',
+                'Origin:'.$this->siteUrl,
+                'Connection:keep-alive',
+                'User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+                'Referer:'.$this->siteUrl.'/clagt/admire/send_admire_mail.php?admire_type=T&admire_category=B&manid='.$manId.'&womanid='.$womanId,
+                'Cookie:PHPSESSID='.$this->cookie,
+            ]
+        );
+
+        // receive server response ...
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_POSTREDIR, 3);
+
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        curl_exec($ch);
+
+        curl_close($ch);
     }
 }
